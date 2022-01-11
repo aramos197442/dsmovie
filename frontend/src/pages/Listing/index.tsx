530e93d9c378d56d29333ac3fc8c7945538fdbbd
aramos197442 <aramos197442@gmail.com>
2022-01-11 16:58:53 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,8 @@
+import Pagination from "components/Navbar/Pagination";
+
 function Listing() {
     return (
-        <h1>Página de Listagem</h1>
-
+        <Pagination />
     )
 }
 export default Listing;
